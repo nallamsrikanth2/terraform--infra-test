d1b0cd675c0efe2cb1099b0b913597d14f724ba5
nallamsrikanth12 <nallamsrikanth2@gmail.com>
2026-06-25 19:36:53 +0530

    terraform

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/nallamsrikanth2/terraform-infra-dev.git//vpc?ref=main"
+    source = "git::https://github.com/nallamsrikanth2/terraform-infra-dev.git?ref=main"
     project_name = var.project_name
     environment =  var.environment
     common_tags = var.common_tags
